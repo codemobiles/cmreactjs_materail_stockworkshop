9d95c6df1253c6f21b6e7a57ffe2867a6436a4b4
codemobiles <chaiyasit.t@gmail.com>
2020-07-15 14:03:00 +0700

    Ep53 - Setup Dialog

--- src/components/pages/Stock.js
+++ src/components/pages/Stock.js
@@ -27,6 +27,12 @@ import ViewColumn from "@material-ui/icons/ViewColumn";
 import { useDispatch, useSelector } from "react-redux";
 import * as stockActions from "./../../actions/stock.action";
 
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
 const tableIcons = {
   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
   Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
@@ -54,6 +60,15 @@ const tableIcons = {
 export default function Stock(props) {
   const dispatch = useDispatch();
   const stockReducer = useSelector(({ stockReducer }) => stockReducer);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   React.useEffect(() => {
     dispatch(stockActions.getProducts());
@@ -173,6 +188,34 @@ export default function Stock(props) {
           ),
         }}
       />
+
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
